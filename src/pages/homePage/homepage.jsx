15a502c58363ff86074b8506bcef2619fb0f3ba6
Akmalov-dev <amirbekdebruyne@gmail.com>
2026-05-24 01:23:52 +0500

    ending mid part

--- src/pages/homePage/homepage.jsx
+++ src/pages/homePage/homepage.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Pishpish from '../homePage/images/pishpish.png'
 import Dizen from '../homePage/images/dizen.jpg'
 import '../homePage/HomePage.css'
+import Brain from '../homePage/images/brain.avif'
+import Medal from '../homePage/images/medal.png'
+import Shake from '../homePage/images/shake.png'
+import Back from '../homePage/images/back.png'
 
 const homepage = () => {
   return (
@@ -17,6 +21,35 @@ const homepage = () => {
       </div>
       <div>
         <img className='img-dizen' src={Dizen} alt="pic" />
+      </div>
+     </div>
+     <p className='f-afzallik'>Afzalliklarimiz</p>
+     <hr />
+
+     <div className='afzallik'>
+      <div className='first-afzallik'>
+        <img className='img-brain' src={Brain} alt="alt" />
+        <p className='f-tezda'>Tezda xizmat ko’rsatish</p>
+        <p className='f-bizning'> Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p>
+      </div>
+        <div className='first-afzallik'>
+        <img className='img-medal' src={Medal} alt="alt" />
+        <p className='f-tezda'>Yetuk mutahasislaimiz</p>
+        <p className='f-bizning'> Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p>
+      </div>
+        <div className='first-afzallik'>
+        <img className='img-brain' src={Shake} alt="alt" />
+        <p className='f-tezda'>Sizga maqul vaqtda</p>
+        <p className='f-bizning'> Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p>
+      </div>
+     </div>
+
+     <div>
+      <img className='img-back' src={Back} alt="alt" />
+      <div className='klapa-font'>
+        <p className='f-klapa'>Klapa va zararli hashorot endi yo’q deb hisoblang !!!</p>
+        <p className='f-bizning1'>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p>
+        <button className='btn-contact'>Bog'lanish</button>
       </div>
      </div>
     </>
